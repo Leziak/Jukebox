f4a594446d31574c47b3ac55a3af6018fd0f9443
TBarta <therestcz@seznam.cz>
2018-06-01 12:20:50 +0200

    database update

--- database/migrations/songs.sql
+++ database/migrations/songs.sql
@@ -2,8 +2,8 @@
 -- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3307
--- Generation Time: Jun 01, 2018 at 01:11 AM
+-- Host: localhost:3306
+-- Generation Time: Jun 01, 2018 at 03:16 AM
 -- Server version: 5.7.22
 -- PHP Version: 7.1.17
 
@@ -44,9 +44,9 @@ CREATE TABLE `songs` (
 --
 
 INSERT INTO `songs` (`id`, `name`, `code`, `author`, `description`, `date`, `url`, `html_url`) VALUES
-(1, 'Ubuntu', 'ur5cqfkA4Y4', NULL, NULL, NULL, NULL, NULL),
-(2, 'Triumph of the nerds', '`ZFUsYXZSMqs&t=354s', NULL, NULL, NULL, NULL, NULL),
-(3, 'Pirates of Silicon Valley', 'GqFYsvxHZOs', NULL, NULL, NULL, NULL, NULL);
+(1, 'One kiss', 'DkeiKbqa02g', 'Calvin Harris, Dua Lipa', 'Calvin Harris & Dua Lipa – One Kiss (Official Video)\r\nListen/download here: http://smarturl.it/One-Kiss\r\n \r\nFollow Calvin Harris Radio (playlist): http://smarturl.it/CalvinHarrisRadio\r\nSubscribe to Calvin\'s channel: http://smarturl.it/CHVevo?IQid=YT', NULL, 'https://www.youtube.com/watch?v=DkeiKbqa02g', ''),
+(2, 'No Tears Left To Cry', 'ffxKSjUwKdU', 'Ariana Grande', 'Director - Dave Meyers\r\nProducer - Nathan Scherrer\r\nEditor - Nick Gilberg\r\nDirector of Photography - Scott Cunningham\r\nProduction Designer - Ethan Tobman\r\nVisual Fx - Buf\r\nColourist - Stefan Sonnenfeld\r\nA FREENJOY PRODUCTION\r\n\r\nMusic video by Ariana Grande performing No Tears Left To Cry. © 2018 Republic Records, a Division of UMG Recordings, Inc.', NULL, 'https://www.youtube.com/watch?v=ffxKSjUwKdU', NULL),
+(3, 'Meant to Be', 'zDo0H8Fm7d0', 'Bebe Rexha', 'Bebe Rexha - Meant to Be (feat. Florida Georgia Line)\r\n\r\nListen now: https://BebeRexha.lnk.to/MeantToBe\r\nSpotify: https://BebeRexha.lnk.to/MeantToBe/Sp...\r\nApple Music: https://BebeRexha.lnk.to/MeantToBe/Ap...\r\niTunes: https://BebeRexha.lnk.to/MeantToBe/iT...\r\n\r\nDirected by: Sophie Muller', NULL, 'https://www.youtube.com/watch?v=zDo0H8Fm7d0', NULL);
 
 --
 -- Indexes for dumped tables
