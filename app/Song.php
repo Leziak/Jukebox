ed8ce04a91480ae5d967ebb51da6db7e16c371b3
Leziak <samo.majoros@gmail.com>
2018-06-01 10:27:15 +0200

    Data manipulation

--- app/Song.php
+++ app/Song.php
@@ -1,7 +1,117 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: samo
- * Date: 1.6.2018
- * Time: 9:22
- */
+
+namespace app;
+
+use \leziak\mvc\db;
+use PDO;
+
+class Song
+{
+    public $id = null;
+    public $name = null;
+    public $code = null;
+    public $author = null;
+    public $description = null;
+    public $date = null;
+    public $url = null;
+    public $html_url = null;
+
+    public static function find($id)
+    {
+        $query = "
+            SELECT *
+            FROM `songs`
+            WHERE `id` = ?
+            ";
+
+        $statement = db::query($query, [
+            $id
+        ]);
+
+        $statement->setFetchMode(PDO::FETCH_CLASS, static::class);
+
+        return $statement->fetch();
+    }
+
+    public static function select($query, $values = [])
+    {
+        $statement = db::query($query, $values);
+
+        $statement->setFetchMode(PDO::FETCH_CLASS, static::class);
+
+        return $statement->fetchAll();
+    }
+
+    public function insert()
+    {
+        $query = "
+            INSERT 
+            INTO `songs`
+            (`name`, `code`, `author`, description`, `date`, `url`, `html_url)
+            VALUES
+            (?, ?, ?, ?, ?, ?, ?)
+        ";
+
+        $values = [
+            $this->name,
+            $this->code,
+            $this->author,
+            $this->description,
+            $this->date,
+            $this->url,
+            $this->html_url,
+        ];
+
+        db::query($query, $values);
+
+        $this->id = db::getConnection()->lastInsertId();
+    }
+
+    public function update()
+    {
+        $query = "
+            UPDATE `product`
+            SET `name` = ?,
+                `code` = ?,
+                `author` = ?,
+                `description` = ?,
+                `date` = ?,
+                `url` = ?,
+                `html_url` = ?,
+            WHERE `id` = ?
+        ";
+
+        $values =  [
+            $this->name,
+            $this->code,
+            $this->author,
+            $this->description,
+            $this->date,
+            $this->url,
+            $this->html_url,
+        ];
+
+        db::query($query, $values);
+    }
+
+    public function delete()
+    {
+        $query = "
+            DELETE
+            FROM `songs`
+            WHERE `id` = ?
+        ";
+
+        db::query($query, [$this->id]);
+
+    }
+
+    public function save()
+    {
+        if($this->id) {
+            $this->update();
+        } else {
+            $this->insert();
+        }
+    }
+}
--- config/app.php
+++ config/app.php
@@ -2,6 +2,6 @@
 
 define('SITE_URL', 'http://www.jukebox.test:8080');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'cbp_eshop');
+define('DB_NAME', 'jukebox');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'rootroot');
