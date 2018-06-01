9530bdf1d818eedabd449b7ee7a56ae9fbc13800
TBarta <therestcz@seznam.cz>
2018-06-01 10:37:30 +0200

    config

--- config/app.php
+++ config/app.php
@@ -1,6 +1,6 @@
 <?php
 
-define('SITE_URL', 'http://www.jukebox.test:8080');
+define('SITE_URL', 'http://www.jukebox.test');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'jukebox');
 define('DB_USER', 'root');
