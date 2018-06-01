b94d90b480f12d612bfbe952d57f07f591d75ff4
TBarta <therestcz@seznam.cz>
2018-06-01 10:40:15 +0200

    config deleted

--- config/app.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('SITE_URL', 'http://www.jukebox.test');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'jukebox');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'rootroot');
